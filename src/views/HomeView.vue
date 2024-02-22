c0c387a2d6e73b3e64959767c52c392fda352762
Adel djahnit <adeldjahnita@gmail.com>
2024-02-22 14:10:30 +0100

    first

---
+++ src/views/HomeView.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="home-container">
+    <h1 class="home-title">DailyDish</h1>
+    <p class="home-introduction">
+      Bienvenue sur DailyDish, votre destination culinaire pour découvrir une variété de plats délicieux et inspirants. Parcourez notre collection de recettes et laissez-vous emporter par un voyage gustatif exceptionnel.
+    </p>
+    
+    <MealSearchPagination />
+  </div>
+</template>
+
+<script>
+import MealSearchPagination from '../components/MealSearchPagination.vue';
+
+export default {
+  name: 'HomeView',
+  components: {
+    MealSearchPagination,
+  },
+};
+</script>
+
+<style scoped>
+/* Styles CSS spécifiques au composant Home */
+.home-container {
+  text-align: center;
+  padding: 20px;
+}
+
+.home-title {
+  font-size: 2.5rem;
+  margin-bottom: 20px;
+}
+
+.home-introduction {
+  font-size: 1.2rem;
+}
+</style>
